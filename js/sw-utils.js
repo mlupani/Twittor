ebc8c0f2a581d83c069cb08c5024d2698a22ec66
Miguel Angel Lupani <mlupani2@gmail.com>
2021-08-22 21:10:48 -0300

    cambio

--- js/sw-utils.js
+++ js/sw-utils.js
@@ -3,7 +3,7 @@ function actualizarDynamic(dynamicCache, req, res){
     if(res.ok){
         return caches.open(dynamicCache).then(cache => {
             cache.put(req, res.clone())
-            return res.clone
+            return res.clone()
         })
     }else{
         return res;
